7eacc620a2aa0fd11f098d3e24432ebf11dbf981
Jo <jkoyama232@gmail.com>
2019-08-28 13:58:07 -0700

    added some incomplete methods

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.uselessmachine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     private Switch useless;
     private ToggleButton killSwitch;
+    private SwitchFunction switchu;
 
 
     @Override
@@ -18,9 +21,64 @@ public class MainActivity extends AppCompatActivity {
         
         wireWidgets();
         setListeners();
+        updateGameDisplay();
     }
 
+    private void updateGameDisplay() {
+
+    }
+
+
     private void setListeners() {
+        useless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    switchu.setAnnoyanceLevel(switchu.getAnnoyanceLevel()+500);
+                    if (switchu.getAnnoyanceLevel()>=5000){
+
+                    }
+                    new CountDownTimer(5000-switchu.getAnnoyanceLevel(),100) {
+                        @Override
+                        public void onTick(long l) {
+                            if (!useless.isChecked()){
+                                cancel();
+                            }
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            useless.setChecked(false);
+
+                        }
+                    }.start();
+                    //switch is turned off after 5 seconds
+                    //increase annoyance by 5
+                }
+
+            }
+
+        });
+        killSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    new CountDownTimer(5000, 100) {
+                        @Override
+                        public void onTick(long l) {
+
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            finish();
+
+                        }
+                    }
+                }
+
+            }
+        });
     }
 
     private void wireWidgets() {
